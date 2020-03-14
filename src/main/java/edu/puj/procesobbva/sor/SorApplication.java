4ea479bc7ac5151450bb1645b4eacce274a162bb
Michael Salgado <salgadosmichael@javeriana.edu.co>
2020-03-14 09:59:56 -0500

    se inicia el proyecto

---
+++ src/main/java/edu/puj/procesobbva/sor/SorApplication.java
@@ -0,0 +1,13 @@
+package edu.puj.procesobbva.sor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/puj/procesobbva/sor/SorApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.puj.procesobbva.sor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
